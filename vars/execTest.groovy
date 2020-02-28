9cf4796c8b965685ce7899261befde2be579e36b
shabbier0473 <55012477+shabbier0473@users.noreply.github.com>
2020-02-28 12:45:51 +0530

    as

---
+++ vars/execTest.groovy
@@ -0,0 +1,26 @@
+def call(String command) {
+	if (command == "test") 
+	{
+pipeline{
+    agent any 
+    parameters{
+        gitParameter branchFilter: 'origin/(.*)', defaultValue: 'origin/master', name: 'BRANCH', type: 'PT_BRANCH'
+    }
+    stages{
+        stage ('master') {
+            when { 
+                expression {GIT_BRANCH == 'origin/master'  }
+            }
+            steps{
+                echo "master"
+            }
+        }
+        stage ('release'){
+            when {
+                expression {GIT_BRANCH == 'origin/release'  }
+            }
+            steps{
+                    echo 'release' }
+        }
+    }
+}
