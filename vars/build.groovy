0b176fa762764258296bfbe96741dddfd4ce75e9
shabbier0473 <55012477+shabbier0473@users.noreply.github.com>
2020-03-02 15:58:37 +0530

    as

--- vars/build.groovy
+++ vars/build.groovy
@@ -4,7 +4,7 @@ def call(String command) {
 	    pipeline {
     agent any
     stages{
-        stage ('hello'){
+        stage ('hi'){
             tools{ maven "MAVEN_HOME"}
             steps{
                 sh "mvn validate"
