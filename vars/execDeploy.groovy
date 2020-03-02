eac19848a9bde5572741ec1904c939f7616a6609
shabbier0473 <55012477+shabbier0473@users.noreply.github.com>
2020-03-02 15:36:06 +0530

    ad

---
+++ vars/execDeploy.groovy
@@ -0,0 +1,15 @@
+def call(String command) {
+	if (command == "deploy") 
+	{
+	    
+pipeline {
+    ageny any
+    stage{
+        stage ('deploy'){
+            tools{ maven "MAVEN_HOME"}
+            steps{
+                sh "mvn deploy"
+            }
+        }
+    }
+}
