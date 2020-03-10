994865bfb31e07703f761fd5453e98fb9125fab7
shabbier0473 <55012477+shabbier0473@users.noreply.github.com>
2020-03-10 17:21:40 +0530

    po

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,14 +1,8 @@
 def call(String command) {
 	if (command == "hello") 
 	{
-    agent any
-    stages{
-        stage ('hi'){
-            tools{ maven "MAVEN_HOME"}
-            steps{
-                sh "mvn validate"
-            }
-        }
-    }
+   node{
+    echo "shabbier"
+   }
 }
 }
