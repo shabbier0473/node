7177ac3ae5226976c1c64bd140d4f8d7c9f3ecea
shabbier0473 <55012477+shabbier0473@users.noreply.github.com>
2020-03-11 19:50:54 +0530

    pol

--- vars/git.groovy
+++ vars/git.groovy
@@ -1,10 +1,19 @@
 def call(String command) {
 if (command == "scm") 
  {
-node{
+pipeline{
+    agent{ label  'maven' } 
+    parameters{
+        gitParameter branchFilter: 'origin/(.*)', defaultValue: 'origin/master', name: 'BRANCH', type: 'PT_BRANCH'
+        gitParameter name: 'TAG',type: 'PT_TAG', selectedValue: 'NONE'
+    }
+    stages{
         stage ('scm'){
+            steps{
                 git branch: 'devlop', credentialsId: 'git', url: 'https://github.com/shabbier0473/time-tracker.git'
+            }
         }
+    }
 }
 }
 }
