7432bfc3675d9fc4732efdcd264689607e3f8ea0
shabbier0473 <55012477+shabbier0473@users.noreply.github.com>
2020-03-11 23:45:50 +0530

    wq

--- vars/step2.groovy
+++ vars/step2.groovy
@@ -3,7 +3,7 @@ if (command == "build")
  {
 node{
     stage('build'){
-        ws ('/home/maven/workspace/project'){
+       {
             def mavenHome = tool name : 'MAVEN_HOME' ,type: 'maven'
             sh "${mavenHome}/bin/mvn install"
         }
