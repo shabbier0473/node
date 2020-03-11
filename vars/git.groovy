7658a5586e0a30ea0ee109e62cb7b2e09e3b785d
shabbier0473 <55012477+shabbier0473@users.noreply.github.com>
2020-03-11 19:54:08 +0530

    qw

--- vars/git.groovy
+++ vars/git.groovy
@@ -1,16 +1,20 @@
 def call(String command) {
 if (command == "scm") 
  {
-pipeline{
+     pipeline{
     agent{ label  'maven' } 
     parameters{
         gitParameter branchFilter: 'origin/(.*)', defaultValue: 'origin/master', name: 'BRANCH', type: 'PT_BRANCH'
         gitParameter name: 'TAG',type: 'PT_TAG', selectedValue: 'NONE'
     }
     stages{
-        stage ('scm'){
+        stage ('validate') {
+            tools{ maven 'MAVEN_HOME' }
+            when { 
+                expression {BRANCH == 'devlop'  }
+            }
             steps{
-                git branch: 'devlop', credentialsId: 'git', url: 'https://github.com/shabbier0473/time-tracker.git'
+                sh 'mvn validate'
             }
         }
     }
