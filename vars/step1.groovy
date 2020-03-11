645189357b050efb2635e49340952ca86ddfe95d
shabbier0473 <55012477+shabbier0473@users.noreply.github.com>
2020-03-11 23:45:26 +0530

    wq

--- vars/step1.groovy
+++ vars/step1.groovy
@@ -3,7 +3,7 @@ if (command == "compile")
  {
 node{
     stage('compile'){
-        ws ('/home/maven/workspace/project'){
+       {
             def mavenHome = tool name : 'MAVEN_HOME' ,type: 'maven'
             sh "${mavenHome}/bin/mvn compile"
         }
