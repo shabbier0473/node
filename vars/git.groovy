6fb9d27f0f70b3d66e2b8e4cfb661593396d9730
shabbier0473 <55012477+shabbier0473@users.noreply.github.com>
2020-03-11 19:47:09 +0530

    po

--- vars/git.groovy
+++ vars/git.groovy
@@ -3,9 +3,7 @@ if (command == "scm")
  {
 node{
         stage ('scm'){
-            steps{
                 git branch: 'devlop', credentialsId: 'git', url: 'https://github.com/shabbier0473/time-tracker.git'
-            }
         }
 }
 }
