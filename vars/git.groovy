26a0ff0730032996d3af9482c7f21f1ee3e9d77c
shabbier0473 <55012477+shabbier0473@users.noreply.github.com>
2020-03-11 19:46:18 +0530

    qw

--- vars/git.groovy
+++ vars/git.groovy
@@ -2,13 +2,11 @@ def call(String command) {
 if (command == "scm") 
  {
 node{
-    stages{
         stage ('scm'){
             steps{
                 git branch: 'devlop', credentialsId: 'git', url: 'https://github.com/shabbier0473/time-tracker.git'
             }
         }
-    }
 }
 }
 }
