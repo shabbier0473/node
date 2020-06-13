bffa8afb2ecbd95db92a391948f2f855f13f9dcb
shabbier0473 <55012477+shabbier0473@users.noreply.github.com>
2020-06-13 16:08:27 +0530

    wq

---
+++ vars/test.groovy
@@ -0,0 +1,14 @@
+def call(String command) {
+	if (command == "test") 
+	{
+        pipeline{
+            agent any
+            tools { maven "MAVEN_HOME" }
+            stages{
+                stage ("test"){
+                    steps{
+                        echo " test "
+                    }
+                }
+            }
+        }
